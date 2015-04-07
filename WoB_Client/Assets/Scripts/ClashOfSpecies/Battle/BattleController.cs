897a2fe14855e9bd90cc03b94ea77560360299fc
Stanley <ticktick58@gmail.com>
2015-04-06 22:58:57 -0700

    added the clash code

---
+++ WoB_Client/Assets/Scripts/ClashOfSpecies/Battle/BattleController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleController : MonoBehaviour {
+	PersistentData attackingData;
+
+	// Use this for initialization
+	void Start () {
+		attackingData = GameObject.Find ("PersistentObject").GetComponent("AttackingData") as PersistentData;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public GameObject searchForEnemy(string unitAttribute) {
+		return null;
+	}
+}
---
+++ WoB_Client/Assets/Scripts/ClashOfSpecies/Selection/PersistentData.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PersistentData : MonoBehaviour {
+	private class UnitData {
+		string name;
+		string type;
+		int hp;
+	}
+
+	private class Defender {
+		string enemyName;
+		string enemyTerrain;
+
+	}
+
+	private class Attacker {
+
+	}
+
+	string enemyName;
+	string enemyTerrain;
+	ArrayList enemyUnitNames;
+	int[] enemyUnitBiomass;
+	ArrayList unitNames;
+	int[] unitBiomass;
+
+	void Awake() {
+		DontDestroyOnLoad (this);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool addUnit(string name) {
+		if (unitNames.Count == 5) {
+			print ("Only 5 unique species allowed");
+			return false;
+		}
+		unitNames.Add (name);
+		return true;
+	}
+
+}
