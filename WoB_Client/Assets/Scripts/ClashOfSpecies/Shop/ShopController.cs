f5fd6645fd39c9c9256a21ce21ded7fdeb599800
Stanley <ticktick58@gmail.com>
2015-04-16 13:09:36 -0700

    ClashShop tab control and some backend

---
+++ WoB_Client/Assets/Scripts/ClashOfSpecies/Shop/ShopController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShopController : MonoBehaviour {
+	GameObject required_object;
+	public Transform c;
+
+	// Use this for initialization
+	void Start () {
+		required_object = GameObject.Find ("Persistent Object");
+		if (required_object == null) {
+			//instantiate the persistent object
+			//print ("NULL");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
