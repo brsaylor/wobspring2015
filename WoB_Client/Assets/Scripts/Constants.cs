224b6863bd2b942bc8d810ba9f312c8b3378596c
Ben Saylor <brsaylor@gmail.com>
2015-04-22 14:25:55 -0700

    Set REMOTE_HOST in client to thecity

--- WoB_Client/Assets/Scripts/Constants.cs
+++ WoB_Client/Assets/Scripts/Constants.cs
@@ -6,7 +6,7 @@ public class Constants {
 	
 	// Constants
 	public static readonly string CLIENT_VERSION = "1.00";
-	public static readonly string REMOTE_HOST = "localhost"; //"localhost";  "smurf.sfsu.edu";
+	public static readonly string REMOTE_HOST = "thecity.sfsu.edu"; //"localhost";  "smurf.sfsu.edu";
 	public static readonly int REMOTE_PORT = 9299;
 	public static readonly float HEARTBEAT_RATE = 1f;
 	
