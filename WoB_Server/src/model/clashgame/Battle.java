add4ea155fa179e8b16e1c20c8180d28adb2e6a3
Dave Kushner <dkushner2@mail.csuchico.edu>
2015-04-22 19:51:36 -0700

    Battle model implemented.

--- WoB_Server/src/model/clashgame/Battle.java
+++ WoB_Server/src/model/clashgame/Battle.java
@@ -8,104 +8,23 @@ import java.sql.Date;
  */
 public class Battle {
     
-	private int battle_id;
-	private int attack_config_id;
-	private int defence_config_id;
-	private Date battle_start;
-	private String battle_outcome;
-    
-	/**
-	 * @param battle_id
-	 * @param attack_config_id
-	 * @param defence_config_id
-	 * @param battle_start
-	 * @param battle_outcome
-	 */
-	public Battle(int battle_id, int attack_config_id, int defence_config_id,
-			Date battle_start, String battle_outcome) {
-		super();
-		this.battle_id = battle_id;
-		this.attack_config_id = attack_config_id;
-		this.defence_config_id = defence_config_id;
-		this.battle_start = battle_start;
-		this.battle_outcome = battle_outcome;
-	}
+	private int battleId;
+	private int attackConfigId;
+	private int defenseConfigId;
+	private Date battleStart;
+	private String outcome;
 
-	public Battle() {
-		// TODO Auto-generated constructor stub
-	}
+    public Battle(int battleId, int attackConfigId, int defenseConfigId, Date battleStart, String outcome) {
+        this.battleId = battleId;
+        this.attackConfigId = attackConfigId;
+        this.defenseConfigId = defenseConfigId;
+        this.battleStart = battleStart;
+        this.outcome = outcome;
+    }
 
-	/**
-	 * @return the battle_id
-	 */
-	public final int getBattle_id() {
-		return battle_id;
-	}
-	
-	/**
-	 * @param battle_id the battle_id to set
-	 */
-	public final void setBattle_id(int battle_id) {
-		this.battle_id = battle_id;
-	}
-	/**
-	 * @return the attack_config_id
-	 */
-	public final int getAttack_config_id() {
-		return attack_config_id;
-	}
-	/**
-	 * @param attack_config_id the attack_config_id to set
-	 */
-	public final void setAttack_config_id(int attack_config_id) {
-		this.attack_config_id = attack_config_id;
-	}
-	/**
-	 * @return the defence_config_id
-	 */
-	public final int getDefence_config_id() {
-		return defence_config_id;
-	}
-	/**
-	 * @param defence_config_id the defence_config_id to set
-	 */
-	public final void setDefence_config_id(int defence_config_id) {
-		this.defence_config_id = defence_config_id;
-	}
-
-	/**
-	 * @return the battle_start
-	 */
-	public final Date getBattle_start() {
-		return battle_start;
-	}
-
-	/**
-	 * @param battle_start the battle_start to set
-	 */
-	public final void setBattle_start(Date battle_start) {
-		this.battle_start = battle_start;
-	}
-
-	/**
-	 * @return the battle_outcome
-	 */
-	public final String getBattle_outcome() {
-		return battle_outcome;
-	}
-
-	/**
-	 * @param battle_outcome the battle_outcome to set
-	 */
-	public final void setBattle_outcome(String battle_outcome) {
-		this.battle_outcome = battle_outcome;
-	}
-
-	
-	
-	
-	
-    
-    
-
+    int getBattleId() { return battleId; }
+    int getAttackConfigId() { return attackConfigId; }
+    int getDefenseConfigId() { return defenseConfigId; }
+    Date getBattleStart() { return battleStart; }
+    String getOutcome() { return outcome; }
 }
