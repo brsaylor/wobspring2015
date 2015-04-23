5cd44735cd917a8a8c0a6c6028d52ec2e6f80e3a
Lev Trubov <ltrubov@gmail.com>
2015-04-22 17:09:45 -0700

    import gamedb fixed

--- WoB_Server/src/db/clashgame/DefenseConfigDAO.java
+++ WoB_Server/src/db/clashgame/DefenseConfigDAO.java
@@ -1,4 +1,5 @@
 package db.clashgame;
+import db.GameDB;
 
 // Java Imports
 import java.sql.Connection;
@@ -10,7 +11,7 @@ import java.sql.Statement;
 
 // Other Imports
 
-import model.clashgame.DefenceConfig;
+import model.clashgame.DefenseConfig;
 import util.Log;
 
 /**
@@ -20,32 +21,32 @@ import util.Log;
  * @author Abhijit
  */
 
-public final class DefenceConfigDAO {
+public final class DefenseConfigDAO {
 
-        private DefenceConfigDAO() {
+        private DefenseConfigDAO() {
         
         }
 
         
         
-        public static DefenceConfig createDefenceConfig(	  int species_1
-        												, int species_1_loc_x
-        												, int species_1_loc_y
+        public static DefenseConfig createDefenseConfig(	  int species_1
+        												, float species_1_loc_x
+        												, float species_1_loc_y
          												, int species_2
-         												, int species_2_loc_x
-         												, int species_2_loc_y
+         												, float species_2_loc_x
+         												, float species_2_loc_y
          												, int species_3
-         												, int species_3_loc_x
-         												, int species_3_loc_y
+         												, float species_3_loc_x
+         												, float species_3_loc_y
          												, int species_4
-         												, int species_4_loc_x
-         												, int species_4_loc_y
+         												, float species_4_loc_x
+         												, float species_4_loc_y
          												, int species_5
-         												, int species_5_loc_x
-         												, int species_5_loc_y
+         												, float species_5_loc_x
+         												, float species_5_loc_y
          												, int player_id
          												, int terrain_id) {
-            DefenceConfig DC = null;
+            DefenseConfig DC = null;
 
             String query = "INSERT INTO `clash_defense_config` "
             						+ "(  `species_1`, `species_1_loc_x`, 'species_1_loc_y`"
@@ -70,24 +71,24 @@ public final class DefenceConfigDAO {
                 con = GameDB.getConnection();
                 pstmt = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
                 pstmt.setInt(1, species_1);
-                pstmt.setInt(2, species_1_loc_x);
-                pstmt.setInt(3, species_1_loc_y);
+                pstmt.setFloat(2, species_1_loc_x);
+                pstmt.setFloat(3, species_1_loc_y);
                 
                 pstmt.setInt(4, species_2);
-                pstmt.setInt(5, species_2_loc_x);
-                pstmt.setInt(6, species_2_loc_y);
+                pstmt.setFloat(5, species_2_loc_x);
+                pstmt.setFloat(6, species_2_loc_y);
                 
                 pstmt.setInt(7, species_3);
-                pstmt.setInt(8, species_3_loc_x);
-                pstmt.setInt(9, species_3_loc_y);
+                pstmt.setFloat(8, species_3_loc_x);
+                pstmt.setFloat(9, species_3_loc_y);
                 
                 pstmt.setInt(10, species_4);
-                pstmt.setInt(11, species_4_loc_x);
-                pstmt.setInt(12, species_4_loc_y);
+                pstmt.setFloat(11, species_4_loc_x);
+                pstmt.setFloat(12, species_4_loc_y);
                 
                 pstmt.setInt(13, species_5);
-                pstmt.setInt(14, species_5_loc_x);
-                pstmt.setInt(15, species_5_loc_y);
+                pstmt.setFloat(14, species_5_loc_x);
+                pstmt.setFloat(15, species_5_loc_y);
                 
 
                 pstmt.setInt(16, player_id);
@@ -99,7 +100,7 @@ public final class DefenceConfigDAO {
 
                 if (rs.next()) {
                     int defence_config_id = rs.getInt(1);
-                    DC = new DefenceConfig(  defence_config_id
+                    DC = new DefenseConfig(  defence_config_id
                     			, species_1
                     			, species_1_loc_x
                     			, species_1_loc_y
@@ -130,9 +131,9 @@ public final class DefenceConfigDAO {
 
     
         
-    public static DefenceConfig getConfig(int player_id) {
+    public static DefenseConfig getConfig(int player_id) {
         
-    	DefenceConfig DC = null;
+    	DefenseConfig DC = null;
 
         String query = "SELECT "
         			+ "    clash_defense_config_id "
@@ -157,7 +158,7 @@ public final class DefenceConfigDAO {
 
             if (rs.next()) {
             	
-                DC = new DefenceConfig();
+                DC = new DefenseConfig();
                 
                 DC.setDefence_config_id(rs.getInt("clash_defense_config_id"));
                 DC.setPlayer_id(rs.getInt("player_id"));
