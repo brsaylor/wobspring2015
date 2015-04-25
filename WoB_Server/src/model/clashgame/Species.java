d59f9e0355b6bd35d816ec9165987a4755416ec8
Dave Kushner <dkushner2@mail.csuchico.edu>
2015-04-24 23:20:39 -0700

    Update species list protocol.

--- WoB_Server/src/model/clashgame/Species.java
+++ WoB_Server/src/model/clashgame/Species.java
@@ -4,9 +4,25 @@ package model.clashgame;
  * Created by dkushner on 4/23/2015.
  */
 public class Species {
+    public static enum Type {
+        PLANT(0),
+        CARNIVORE(1),
+        HERBIVORE(2),
+        OMNIVORE(3);
+
+        private final int value;
+        private Type(int value) {
+            this.value = value;
+        }
+        public int getValue() {
+            return this.value;
+        }
+    }
+
     public int speciesId;
+    public String name;
     public int price;
-    public int type;
+    public Type type;
     public String description;
     public int attackPoints;
     public int hitPoints;
