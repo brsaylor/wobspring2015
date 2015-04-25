a7d519440936005236a52f6944bdaed759687c09
Ben Saylor <brsaylor@gmail.com>
2015-04-24 22:01:31 -0700

    Update battle model and RequestClashEndBattle on server

--- WoB_Server/src/net/request/clashgame/RequestClashEndBattle.java
+++ WoB_Server/src/net/request/clashgame/RequestClashEndBattle.java
@@ -10,22 +10,39 @@ import java.io.IOException;
 import net.request.GameRequest;
 import net.response.clashgame.ResponseClashEndBattle;
 import util.DataReader;
+import java.util.Date;
+
+import db.clashgame.BattleDAO;
+import model.clashgame.Battle;
 
 /**
  *
  * @author lev
  */
-public class RequestClashEndBattle extends GameRequest{
-    private boolean won;
+public class RequestClashEndBattle extends GameRequest {
+    
+    Battle.Outcome outcome;
     
     @Override
     public void parse(DataInputStream dataInput) throws IOException {
-        won = DataReader.readBoolean(dataInput);
+        int value = DataReader.readInt(dataInput);
+        
+        if (value == 0) {
+            outcome = Battle.Outcome.WIN;
+        } else if (value == 1) {
+            outcome = Battle.Outcome.LOSE;
+        } else {
+            outcome = Battle.Outcome.DRAW;
+        }
     }
 
     @Override
     public void process() throws Exception {
         //record state in db
+        Battle battle = BattleDAO.findActiveByPlayer(client.getPlayer().getID());
+        battle.outcome = outcome;
+        battle.timeEnded = new Date();
+        BattleDAO.save(battle);
         
         ResponseClashEndBattle response = new ResponseClashEndBattle();
         client.add(response);
