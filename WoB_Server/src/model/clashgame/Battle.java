60291d7ad78c3d29c09f21564f2d9fea76d78f9d
Dave Kushner <dkushner2@mail.csuchico.edu>
2015-04-25 05:05:37 -0700

    Mergergergerg.

--- WoB_Server/src/model/clashgame/Battle.java
+++ WoB_Server/src/model/clashgame/Battle.java
@@ -7,9 +7,9 @@ import java.util.Date;
  * @author Abhijit
  */
 public class Battle {
-	public int id;
-	public int attackConfigId;
-	public int defenseConfigId;
+	public Integer id;
+	public Integer attackConfigId;
+	public Integer defenseConfigId;
 	public Date battleStart;
 	public String outcome;
 }
