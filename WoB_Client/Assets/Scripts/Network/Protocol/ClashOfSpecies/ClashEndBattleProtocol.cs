97e4148a0990005897474856e9c6ff1681b40cb6
Lev Trubov <ltrubov@gmail.com>
2015-05-01 19:25:53 -0700

    documentation for end battle protocol too

--- WoB_Client/Assets/Scripts/Network/Protocol/ClashOfSpecies/ClashEndBattleProtocol.cs
+++ WoB_Client/Assets/Scripts/Network/Protocol/ClashOfSpecies/ClashEndBattleProtocol.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
+/// <summary>
+/// Used to end the battle
+/// </summary>
 public class ClashEndBattleProtocol {
 
 	public enum BattleResult{
@@ -9,7 +12,11 @@ public class ClashEndBattleProtocol {
 		LOSS,
 		DRAW
 	}
-	
+
+	/// <summary>
+	/// Creates a request containing battle results
+	/// </summary>
+	/// <param name="res">The result</param>
 	public static NetworkRequest Prepare(BattleResult res) {
 		NetworkRequest request = new NetworkRequest(NetworkCode.CLASH_END_BATTLE);
 		request.AddInt32((int)res);
@@ -17,7 +24,11 @@ public class ClashEndBattleProtocol {
 		
 		return request;
 	}
-	
+
+	/// <summary>
+	/// Fills the response object with new credit balance
+	/// </summary>
+	/// <param name="dataStream">the input stream</param>
 	public static NetworkResponse Parse(MemoryStream dataStream) {
 		ResponseClashEndBattle response = new ResponseClashEndBattle();
 		response.credits = DataReader.ReadInt(dataStream);
@@ -25,7 +36,14 @@ public class ClashEndBattleProtocol {
 	}
 }
 
+/// <summary>
+/// Stores the new credit balance (changes depending on outcome of battle)
+/// </summary>
 public class ResponseClashEndBattle : NetworkResponse {
+	/// <summary>
+	/// Gets or sets the new credit balance
+	/// </summary>
+	/// <value>The balance.</value>
 	public int credits {get; set;}
 
 	public ResponseClashEndBattle() {
