2d05d57d8f4ca574e7e71c580b7e99026c7c319a
Ben Saylor <brsaylor@gmail.com>
2015-05-08 17:21:08 -0700

    Fix death logic, add lots of debug statements

--- WoB_Client/Assets/Scripts/ClashOfSpecies/Battle/ClashBattleUnit.cs
+++ WoB_Client/Assets/Scripts/ClashOfSpecies/Battle/ClashBattleUnit.cs
@@ -31,14 +31,14 @@ public class ClashBattleUnit : MonoBehaviour {
 	
 	void Update () {
 		timer += Time.deltaTime;
-		if (currentHealth <= 0.0f) {
-			Die();		
-		}
         if (!target) {
-						Idle ();
-				} else if ((target.currentHealth >= 0) && (timer >= timeBetweenAttacks) && (currentHealth >= 0.0f)) {
-						Attack ();
-				} else if (target.currentHealth <= 0) {
+			Debug.Log ("idling", gameObject);
+			Idle ();
+		} else if ((target.currentHealth > 0) && (timer >= timeBetweenAttacks) && (currentHealth >= 0.0f)) {
+			Debug.Log ("attacking", gameObject);
+			Attack ();
+		} else if (target.currentHealth <= 0) {
+			Debug.Log ("target dead", gameObject);
 			target=null;		
 		}
 	}
@@ -58,11 +58,12 @@ public class ClashBattleUnit : MonoBehaviour {
             if (agent.remainingDistance < 1.0f) {
                 // TODO: Attack animation.
                 //Added by Omar triggers Attacking animation
+				Debug.Log(species.name + " attacking " + target.species.name);
                 if (anim != null) {
-                anim.SetTrigger("Attacking");
+                	anim.SetTrigger("Attacking");
                 }
                 // TODO: Deliver damage.
-                target.TakeDamage(species.attack);
+                target.TakeDamage(species.attack, this);
             }else{
 				if (anim != null) {
 					anim.SetTrigger("Walking");
@@ -78,6 +79,10 @@ public class ClashBattleUnit : MonoBehaviour {
 	}
 
     void TakeDamage(int damage, ClashBattleUnit source = null) {
+		Debug.Log (tag + " " + species.name + " taking " + damage + " damage from " + source.tag + " " + source.species.name);
         currentHealth = Mathf.Max(0, currentHealth - damage);
+		if (currentHealth == 0) {
+			Die();
+		}
     }
 }
